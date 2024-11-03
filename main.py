79cb318e50959e3604d7614a650811a2907e9d7d
Ömer Duran <me@omerduran.dev>
2024-11-03 19:03:47 +0100

    feat(main): change poetry to uv

--- main.py
+++ main.py
@@ -22,13 +22,11 @@ from src.etc.exchange_rates import currency_send
 from src.etc.long_term_performance import analyze_long_term_stock
 from src.us.us_open_close import us_open, us_close
 
-# LOGGER CONFIG
 OK_LEVEL_NUM = 22  
 START_LEVEL_NUM = 21
 logging.addLevelName(OK_LEVEL_NUM, "OK")
 logging.addLevelName(START_LEVEL_NUM, "START")
 
-# Define a method to log at the OK level
 def ok(self, message, *args, **kwargs):
     if self.isEnabledFor(OK_LEVEL_NUM):
         self._log(OK_LEVEL_NUM, message, args, **kwargs)
@@ -44,51 +42,50 @@ logging.basicConfig(filename='yatirimbot.log', level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s')
 logger = logging.getLogger(__name__)
 
-# def is_weekday():
-#     """Check if the current day is a weekday."""
-#     return datetime.now(pytz.timezone("Europe/Istanbul")).weekday() < 5
+def is_weekday():
+    """Check if the current day is a weekday."""
+    return datetime.now(pytz.timezone("Europe/Istanbul")).weekday() < 5
 
-# # Daily tasks
-# schedule.every().day.at("06:30", "Europe/Istanbul").do(crypto_send)
-# schedule.every().day.at("11:00", "Europe/Istanbul").do(bist_stock_by_time)
-# schedule.every().day.at("15:00", "Europe/Istanbul").do(bist_stock_by_time)
-# schedule.every().day.at("17:30", "Europe/Istanbul").do(analyze_long_term_stock)
-# schedule.every().day.at("19:00", "Europe/Istanbul").do(bist_stock_by_time)
-# schedule.every().day.at("23:49", "Europe/Istanbul").do(analyze_long_term_stock)
+schedule.every().day.at("06:30", "Europe/Istanbul").do(crypto_send)
+schedule.every().day.at("11:00", "Europe/Istanbul").do(bist_stock_by_time)
+schedule.every().day.at("15:00", "Europe/Istanbul").do(bist_stock_by_time)
+schedule.every().day.at("17:30", "Europe/Istanbul").do(analyze_long_term_stock)
+schedule.every().day.at("19:00", "Europe/Istanbul").do(bist_stock_by_time)
+schedule.every().day.at("23:49", "Europe/Istanbul").do(analyze_long_term_stock)
 
-# # Weekday tasks
-# schedule.every().day.at("10:17", "Europe/Istanbul").do(send_bist_open).tag("weekday")
-# schedule.every().day.at("10:20", "Europe/Istanbul").do(halka_arz).tag("weekday")
-# schedule.every().day.at("10:30", "Europe/Istanbul").do(gold_price).tag("weekday")
-# schedule.every().day.at("11:30", "Europe/Istanbul").do(analyze_silver_prices).tag("weekday")
-# schedule.every().day.at("12:30", "Europe/Istanbul").do(currency_send).tag("weekday")
-# schedule.every().day.at("13:30", "Europe/Istanbul").do(lambda: commodity_price("NG=F", "Doğal Gaz")).tag("weekday")
-# schedule.every().day.at("16:00", "Europe/Istanbul").do(bist30_change).tag("weekday")
-# schedule.every().day.at("16:30", "Europe/Istanbul").do(gold_price).tag("weekday")
-# schedule.every().day.at("16:46", "Europe/Istanbul").do(us_open).tag("weekday")
-# schedule.every().day.at("18:00", "Europe/Istanbul").do(crypto_send).tag("weekday")
-# schedule.every().day.at("18:17", "Europe/Istanbul").do(send_bist_close).tag("weekday")
-# schedule.every().day.at("19:30", "Europe/Istanbul").do(bist30_change).tag("weekday")
-# schedule.every().day.at("20:00", "Europe/Istanbul").do(lambda: commodity_price("CL=F", "Ham Petrol")).tag("weekday")
-# schedule.every().day.at("20:30", "Europe/Istanbul").do(bist30_change).tag("weekday")
-# schedule.every().day.at("22:16", "Europe/Istanbul").do(bist_comp).tag("weekday")
-# schedule.every().day.at("23:16", "Europe/Istanbul").do(us_close).tag("weekday")
-# schedule.every().day.at("23:30", "Europe/Istanbul").do(lambda: commodity_price("HO=F", "Kalorifer Yakıtı")).tag("weekday")
+
+schedule.every().day.at("10:17", "Europe/Istanbul").do(send_bist_open).tag("weekday")
+schedule.every().day.at("10:20", "Europe/Istanbul").do(halka_arz).tag("weekday")
+schedule.every().day.at("10:30", "Europe/Istanbul").do(gold_price).tag("weekday")
+schedule.every().day.at("11:30", "Europe/Istanbul").do(analyze_silver_prices).tag("weekday")
+schedule.every().day.at("12:30", "Europe/Istanbul").do(currency_send).tag("weekday")
+schedule.every().day.at("13:30", "Europe/Istanbul").do(lambda: commodity_price("NG=F", "Doğal Gaz")).tag("weekday")
+schedule.every().day.at("16:00", "Europe/Istanbul").do(bist30_change).tag("weekday")
+schedule.every().day.at("16:30", "Europe/Istanbul").do(gold_price).tag("weekday")
+schedule.every().day.at("16:46", "Europe/Istanbul").do(us_open).tag("weekday")
+schedule.every().day.at("18:00", "Europe/Istanbul").do(crypto_send).tag("weekday")
+schedule.every().day.at("18:17", "Europe/Istanbul").do(send_bist_close).tag("weekday")
+schedule.every().day.at("19:30", "Europe/Istanbul").do(bist30_change).tag("weekday")
+schedule.every().day.at("20:00", "Europe/Istanbul").do(lambda: commodity_price("CL=F", "Ham Petrol")).tag("weekday")
+schedule.every().day.at("20:30", "Europe/Istanbul").do(bist30_change).tag("weekday")
+schedule.every().day.at("22:16", "Europe/Istanbul").do(bist_comp).tag("weekday")
+schedule.every().day.at("23:16", "Europe/Istanbul").do(us_close).tag("weekday")
+schedule.every().day.at("23:30", "Europe/Istanbul").do(lambda: commodity_price("HO=F", "Kalorifer Yakıtı")).tag("weekday")
 
 def main():
     """Run the main scheduling loop."""
     keep_alive()
+    crypto_send()
     logger = logging.getLogger(__name__)
     logger.critical('Script Started')
-    # # Run weekday jobs only on weekdays
-    # for job in schedule.get_jobs():
-    #     if "weekday" in job.tags:
-    #         job.run = lambda job=job: job() if is_weekday() else None
 
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
-    bist_stock_by_time()
+    for job in schedule.get_jobs():
+        if "weekday" in job.tags:
+            job.run = lambda job=job: job() if is_weekday() else None
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
